0155ed5634f127be618005bd2b788de6cacde870
İlayda Balık <ilayda.blk@gmail.com>
2023-05-29 23:27:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ilaydabalikhw1/Variables.kt
@@ -0,0 +1,70 @@
+package com.example.ilaydabalikhw1
+
+
+fun main(){
+
+    val sehir = "Istanbul"
+    val ulke = "Türkiye"
+    val telefon = "+90 543 266 68 57"
+    val postaKodu = 34716
+    val email = "ilayda.blk@gmail.com"
+    val meslek = "Elektrik Elektronik Mühendisi"
+    val stokMiktari = 29
+    val musteriAdi = "İlayda Balık"
+    val bakiye = 34.42
+    val dogumGunu = "23.09.1994"
+    val maas = 50.000
+    val medeniDurum = true
+    val urunYorum = "Başarılı"
+    val odemeTarihi = "11.06.2023"
+    val odeme = true
+    val siparisAdeti = 7
+    val arabaModeli = " KIA NERO"
+    val kitapAdi = "Tutunamayanlar"
+    val yayinlanmaTarihi = 1972
+    val indirimMiktari = 0.30
+    val odaSayisi = "5+1"
+    val enlem = "00-54S"
+    val boylam = "089-36W"
+    val urunAdi = "Tekne"
+    val yemekFiyati = 99.95
+    val marka = "Zara"
+    val muzikAdı = "Indie"
+    val videoSuresi = 65.0
+    val urunPuani = 4.7
+    val resimAdı = "İnci Küpeli Kız"
+    val dosyaFormati = ".jpeg"
+    val renk = "Kırmızı"
+    val renkKodu = "#ff0000"
+    val telefonModeli = "Iphone 14"
+    val ekranBoyutu = 4.7
+    val agırlık = 56.0
+    val ulusalGun = "Cumhuriyet Bayramı"
+    val tatilGunu = "1 Ocak"
+    val rezervasyonTarihi = "27.06.2023"
+    val sokakAdi = "Behiçbey Sok."
+    val otobusHatti = "500T"
+    val kalanDakika = 5.76
+    val takipKodu = "234FTY4567"
+    val kuponSuresi = 3
+    val kuponKodu = "QWR231TT"
+    val faturaAdresi = true
+
+
+
+    println("Yaşadağım şehir $sehir 'dur.")
+    println("$sehir $ulke de bulunur.")
+    println("$telefon benim telefon numaram")
+    println("Rasimpaşa Mahallesi'nin posta kodu: $postaKodu")
+    println("Mail adresim : $email")
+    println("Mesleğim : $meslek")
+
+
+
+
+
+
+
+
+}
+
